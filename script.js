842e70766921310fd9a8d8f371f7432e4a9ee260
James Tlaloc <jamestlaloc.dev@gmail.com>
2026-02-13 18:19:04 -0800

    Try out ternary instead of if...else just because.

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 300);
+  let randomNumber = Math.floor(Math.random() * 30);
 
-  if (randomNumber <= 100) {
-    return "Rock";
-  } else if (randomNumber > 100 && randomNumber <= 200){
-    return "Paper";
-  } else {
-    return "Scissors";
-  }
-}
+  return (randomNumber <= 10) ? "Rock" :
+  (randomNumber > 10 && randomNumber<= 20) ? "paper" : "Scissors";
+
+
+  // if (randomNumber <= 10) {
+  //   return "Rock";
+  // } else if (randomNumber > 10 && randomNumber <= 20){
+  //   return "Paper";
+  // } else {
+  //   return "Scissors";
+  // }
+};
+
+console.log(getComputerChoice());
