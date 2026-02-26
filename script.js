d328a076a5d18cd08fe9e7801bb7005d7464b1ba
James Tlaloc <jamestlaloc.dev@gmail.com>
2026-02-25 18:59:06 -0800

    Create welcome section.

--- script.js
+++ script.js
@@ -1,62 +1,63 @@
-function getComputerChoice() {
-  // Create an array of choices.
-  const choices = ["Rock", "Paper", "Scissors"];
+const buttons = document.querySelectorAll("btn");
+console.log(buttons);
 
-  //Declare a random choice variable and assign it to the value that grabs a random number based on the length of the array of choices.
+// function getComputerChoice() {
+//   const choices = ["Rock", "Paper", "Scissors"];
 
-  //Math.random() generates a random floating number between 0 and 1. When multiplied by a number, it creates a max number.
+//   let randomChoice = Math.floor(Math.random() * choices.length);
 
-  //Math.floor() then rounds that number down so that we get a whole number.
-  let randomChoice = Math.floor(Math.random() * choices.length);
+//   return choices[randomChoice].toLowerCase();
+// };
 
-  // We return the random index that is chosen via the randomChoice variable, and using brackets to access that index from the choices array.
-  return choices[randomChoice].toLowerCase();
-};
+// function playGame() {
+//   let computerScore = 0;
+//   let humanScore = 0;
+// }
 
-function getHumanChoice() {
-  let humanChoice = prompt("Choose: Rock, Paper, or Scissors?");
-  return humanChoice.toLowerCase();
-};
-
-function playGame() {
-  let computerScore = 0;
-  let humanScore = 0;
-
-  // for(let i = 0; i < 7; i++){
-  //   const humanChoice = getHumanChoice();
-  //   const computerChoice = getComputerChoice();
-  //   playRound(humanChoice, computerChoice);
-  //   console.log(`Your Score: ${humanScore}`);
-  //   console.log(`Computer's Score: ${computerScore}`);
-  //   console.log("---------------------")
-  // }
-
-  function playRound(humanChoice, computerChoice){
+// function playRound(humanChoice){
+//   getComputerChoice();
+//   playerSelection();
+//   console.log(humanChoice);
     
-  console.log(`You: ${humanChoice}`);
-  console.log(`Computer: ${computerChoice}`);
-    if(humanChoice === "paper" && computerChoice === "rock"){
-      humanScore++;
-      console.log("You win!!! Paper beats Rock.");
-    } else if (humanChoice === "rock" && computerChoice === "scissors"){
-      humanScore++;
-      console.log("You win!!! Rock beats Scissors."); 
-    } else if (humanChoice === "scissors" && computerChoice === "paper"){
-      humanScore++;
-      console.log("You win!!! Scissors beats Paper.");
-    } else if (computerChoice === "paper" && humanChoice === "rock"){
-      computerScore++;
-      console.log("You lose! Paper beats Rock.");
-    } else if (computerChoice === "rock" && humanChoice === "scissors"){
-      computerScore++;
-      console.log("You lose! rock beats scissors.");    
-    } else if (computerChoice === "scissors" && humanChoice === "paper"){
-      computerScore++;
-      console.log("You lose! Scissors beats Paper.");
-    } else {
-      console.log("It's a draw!")
-    }
-};
-}
+//   console.log(`You chose: ${humanChoice}`);
+//   console.log(`Computer: ${computerChoice}`);
+//     if(humanChoice === "paper" && computerChoice === "rock"){
+//       humanScore++;
+//       console.log("You win!!! Paper beats Rock.");
+//     } else if (humanChoice === "rock" && computerChoice === "scissors"){
+//       humanScore++;
+//       console.log("You win!!! Rock beats Scissors."); 
+//     } else if (humanChoice === "scissors" && computerChoice === "paper"){
+//       humanScore++;
+//       console.log("You win!!! Scissors beats Paper.");
+//     } else if (computerChoice === "paper" && humanChoice === "rock"){
+//       computerScore++;
+//       console.log("You lose! Paper beats Rock.");
+//     } else if (computerChoice === "rock" && humanChoice === "scissors"){
+//       computerScore++;
+//       console.log("You lose! rock beats scissors.");    
+//     } else if (computerChoice === "scissors" && humanChoice === "paper"){
+//       computerScore++;
+//       console.log("You lose! Scissors beats Paper.");
+//     } else {
+//       console.log("It's a draw!")
+//     }
+// };
 
-playGame();
+// const rock = document.querySelector('.rock');
+// const paper = document.querySelector('.paper');
+// const scissors = document.querySelector('.scissors');
+
+
+// rock.addEventListener('click', () => {
+//   humanChoice = 'rock';
+//   playRound();
+// });
+// paper.addEventListener('click', () => {
+//   humanChoice = 'paper';
+//   playRound();
+// });
+// scissors.addEventListener('click', () => {
+//   humanChoice = 'scissors';
+//   playRound();
+// });
